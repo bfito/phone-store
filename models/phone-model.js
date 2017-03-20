ccb73a21d73efb706d6c7340f536f7b74869b341
JPZ <jeanpaulzune@gmail.com>
2017-03-20 10:04:53 -0400

    Added phone.model

---
+++ models/phone-model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const phoneSchema = new Schema({
+  brand: {
+    type: String,
+    required: [true, 'The phone is required']
+  },
+  name:  {
+    type: String,
+    required: [true, 'The phone name is requried']
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  specs: {
+    type: Array,
+    default: []
+  }
+});
+
+const Phone = mongoose.model('Phone', phoneSchema);
+
+module.exports = Phone;
